1e919833758c643da8ce9beb1ca46d813499a96c
Donnie Wang <donniewang@gmail.com>
2017-01-12 20:17:51 -0800

    Added british accent to voice.

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.get('/trello-voice', function(request, response) {
     var header = '<?xml version="1.0" encoding="UTF-8"?><Response>';
     var footer = '</Response>';
     var lines = result.map(function(card) {
-      return "<Say voice='woman'>" + card.name.encodeHTML() + "</Say>";
+      return "<Say voice='woman' language='en-GB'>" + card.name.encodeHTML() + "</Say>";
     });
 
     response.send(header + lines.join('') + footer);
