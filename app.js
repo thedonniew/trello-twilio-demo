844b7813ded30c8c0df0e3503ee1d738f9468298
Donnie Wang <donniewang@gmail.com>
2017-01-16 14:33:34 -0800

    Added /voice route for mp3.

--- app.js
+++ app.js
@@ -54,6 +54,24 @@ app.get('/', function(request, response) {
     response.render('index');
 });
 
+app.post('/voice', (req, res) => {
+  // Set the url of the song we are going to play
+  let songUrl = 'http://ocrmirror.org/files/music/remixes/Street_Fighter_2_Guile%27s_Theme_Goes_with_Metal_OC_ReMix.mp3'
+
+  // Generate a TwiML response
+  let twiml = new twilio.TwimlResponse();
+
+  // Play Guile's theme over the phone.
+  twiml.play(songUrl);
+
+  // Set the response type as XML.
+  res.header('Content-Type', 'text/xml');
+
+  // Send the TwiML as the response.
+  res.send(twiml.toString());
+
+});
+
 // trello route that will serve up an xml file
 app.get('/trello-voice', function(request, response) {
   trello.getCardsOnList(myList, function(error, result) {
